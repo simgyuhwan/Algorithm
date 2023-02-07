b1c34731acebd68424bf5d4172a08df73981d175
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-02-07 22:52:27 +0900

    대소문자 변환

--- algorithm/src/main/java/com/algorithm/winner/lecture/대소문자변환.java
+++ algorithm/src/main/java/com/algorithm/winner/lecture/대소문자변환.java
@@ -1,4 +1,20 @@
 package com.algorithm.winner.lecture;
 
+import java.util.Scanner;
+
 public class 대소문자변환 {
+    public static String solution(String str) {
+        String answer = "";
+        for(char x : str.toCharArray()) {
+            if(x >= 97 && x<= 122) answer+=(char)(x-32);
+            else answer += (char)(x+32);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(solution(input));
+    }
 }
